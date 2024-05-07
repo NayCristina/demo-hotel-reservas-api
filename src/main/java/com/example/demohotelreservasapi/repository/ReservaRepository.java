e953e5029cfcdf84149da927bbb588acebf5330e
NayCristina <161977020+NayCristina@users.noreply.github.com>
2024-05-06 22:02:59 -0300

    altera validação de reserva

---
+++ src/main/java/com/example/demohotelreservasapi/repository/ReservaRepository.java
@@ -0,0 +1,20 @@
+package com.example.demohotelreservasapi.repository;
+
+import com.example.demohotelreservasapi.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+
+        List<Reserva> findByStatus(String status);
+
+        @Query("SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END FROM Reserva r WHERE r.quartoId = :quartoId AND r.hotelId = :hotelId AND r.checkIn = :checkIn")
+        boolean reservasDuplicadas(@Param("quartoId") int quartoId, @Param("hotelId") int hotelId, @Param("checkIn") LocalDate checkIn);
+
+}
---
+++ src/main/java/com/example/demohotelreservasapi/service/ReservaService.java
@@ -0,0 +1,58 @@
+package com.example.demohotelreservasapi.service;
+
+import com.example.demohotelreservasapi.entity.Reserva;
+import com.example.demohotelreservasapi.repository.ReservaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ReservaService {
+
+    private final ReservaRepository reservaRepository;
+
+    @Transactional(readOnly = true)
+    public List<Reserva> buscarTodos() {
+        List<Reserva> reservas = reservaRepository.findAll();
+        if (reservas.isEmpty()) {
+            throw new EntityNotFoundException("Nenhuma reserva encontrada");
+        }
+        return reservas;
+    }
+
+    @Transactional(readOnly = true)
+    public Reserva buscarPorId(int id){
+        return reservaRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Reserva id=%s não encontrado", id))
+        );
+    }
+    @Transactional
+    public List<Reserva> buscarPorStatus(String status) {
+        return reservaRepository.findByStatus(status);
+    }
+
+
+
+    @Transactional
+    public Reserva criarReserva(Reserva reserva) {
+        LocalDate checkIn = reserva.getCheckIn();
+        LocalDate checkOut = reserva.getCheckOut();
+        int quartoId = reserva.getQuartoId();
+        int hotelId = reserva.getHotelId();
+
+        if (reservaRepository.reservasDuplicadas(quartoId, hotelId, checkIn)) {
+            throw new ReservaDuplicadaException("Já existe uma reserva para o mesmo quarto, hotel e data.");
+        }
+
+        if (checkOut.isBefore(checkIn)) {
+            throw new IllegalArgumentException("A data de check-out não pode ser antes da data de check-in.");
+        }
+        reserva.setStatus("ativa");
+        return reservaRepository.save(reserva);
+    }
+}
